9cfdafb7ea45e1e2c323b18c20b3015c6359c3ee
loctetgenius <sagnamoussa546@gmail.com>
2024-03-28 10:40:06 +0000

    Question 2 instanciation d'un objet de la classe Voiture

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
 //Creation de la classe voiture
     class Voiture {
-
+        
+        //
         public $marque;
         public $modele;
         public $kilometrage;
         public $annee;
         public $couleur;
+        
+        //Appel de la Fonction CONSTRUCT pour pouvoir initialiser 
+
+        public function __construct($marque, $modele, $kilometrage, $annee,$couleur){
+            
+            $this->marque = $marque;
+            $this->modele = $modele;
+            $this->kilometrage = $kilometrage;
+            $this->annee = $annee;
+            $this->couleur = $couleur;
+        }
+
 
         // Méthode qui affiche les détails de la voiture
         public function afficherDetails(){
@@ -22,7 +35,15 @@
         }
 
     }
+    
 
+    // Création de l'objet voiture1 qui est une instance de Voiture
+
+    $voiture1= new Voiture("JEEP", "tucson", "500km/h", "2008", "bleu");
+
+    // Appel à la méthode afficherDétails.
+    
+    $voiture1->afficherDetails();
 
 
 
